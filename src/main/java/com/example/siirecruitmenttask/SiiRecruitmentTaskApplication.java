ea59fa1e7c5cc71fd5dbc8ea893947e968233f3d
Kekto <lekto8814@gmail.com>
2024-05-06 16:31:54 +0200

    git-init

---
+++ src/main/java/com/example/siirecruitmenttask/SiiRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.siirecruitmenttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiRecruitmentTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiiRecruitmentTaskApplication.class, args);
+    }
+
+}
